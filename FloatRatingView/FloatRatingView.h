1d26933220dbc14958fe80f36666a59caf3e6ccd
Kevin Kwon <k@hellomarket.com>
2019-06-11 16:05:45 +0900

    cathage init

---
+++ FloatRatingView/FloatRatingView.h
@@ -0,0 +1,19 @@
+//
+//  FloatRatingView.h
+//  FloatRatingView
+//
+//  Created by kevin on 11/06/2019.
+//  Copyright © 2019 kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatRatingView.
+FOUNDATION_EXPORT double FloatRatingViewVersionNumber;
+
+//! Project version string for FloatRatingView.
+FOUNDATION_EXPORT const unsigned char FloatRatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatRatingView/PublicHeader.h>
+
+
